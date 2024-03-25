6ad9809dced756a5c77ec36d48afb130352d36cd
hackerhihello <hackerhihello123@gmail.com>
2024-03-25 11:13:38 +0530

    updatecode

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ mongoose.connect('mongodb://localhost:27017/studentBlogDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+// MongoDB Connection
+// const mongoURI = 'mongodb://localhost:27017/studentBlogDB';
+// mongoose.connect(mongoURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// }).then(() => {
+//     console.log('MongoDB Connected');
+// }).catch(err => console.log(err));
 
 // Define student schema
 const studentSchema = new mongoose.Schema({
--- src/components/StudentDashboard.js
+++ src/components/StudentDashboard.js
@@ -7,7 +7,6 @@ const StudentDashboard = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-   
     const userEmail = sessionStorage.getItem('email');
     if (!userEmail) {
       console.error('User is not logged in');
@@ -18,40 +17,51 @@ const StudentDashboard = () => {
     fetch(`http://localhost:5000/api/student/blogs/${userEmail}`)
       .then(response => response.json())
       .then(data => {
-       
         const acceptedBlogs = data.filter(blog => blog.status === 'accepted');
         setBlogs(acceptedBlogs);
       })
       .catch(error => console.error('Error fetching blogs:', error));
   }, []);
-  
+
   const handleCreateBlog = () => {
     navigate('/create-blog');
   };
+
   const handleViewBlogs = () => {
     navigate('/view-blogs');
   };
-  const handleCancleBlogs = () => {
+
+  const handleCancelBlogs = () => {
     navigate('/cancel-blogs');
   };
 
-
-  // console.log(blogs);
   return (
     <div className="container">
       <h2 className="mt-4">Student Dashboard</h2>
       <div className="d-flex justify-content-end mt-2">
         <button className="btn btn-primary me-2" onClick={handleCreateBlog}>Create Blog</button>
         <button className="btn btn-primary me-2" onClick={handleViewBlogs}>View Blogs</button>
-        <button className="btn btn-primary" onClick={handleCancleBlogs}>Cancel Blogs</button>
+        <button className="btn btn-primary" onClick={handleCancelBlogs}>Cancel Blogs</button>
       </div>
-      <ul className="list-group mt-4">
-        {blogs.map(blog => (
-          <li key={blog._id} className="list-group-item">
-            <Link to={`/blogs/${blog._id}`}>{blog.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <div className="row mt-4">
+        <div className="col-md-8">
+          <ul className="list-group">
+            {blogs.map(blog => (
+              <li key={blog._id} className="list-group-item">
+                <Link to={`/blogs/${blog._id}`}>{blog.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-md-4">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Advertisement</h5>
+              <p className="card-text">Here goes your advertisement content. You can add images, text, or links to promote your products or services.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
